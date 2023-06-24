315dfb51a657963064cd92b13790ea8f79f885b6
gfCrova <giancrova.cl5@gmail.com>
2023-06-24 02:27:25 -0300

    add Routes and css to App comp.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import MoviesGrid from './components/MoviesGrid';
-import styles from './css/App.module.css'
+import { HomePage } from "./pages/HomePage";
+import { MovieDetails } from "./pages/MovieDetails.jsx";
+import styles from "./App.module.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-function App() {
+export const App = () => {
   return (
-    <div>
+    <Router>
       <header className={styles.appHeader}>
-        <h1>Titulo</h1>
-        <MoviesGrid/>
+        <Link className={styles.linkHeader} to="/">
+          <h1>Movies</h1>
+        </Link>
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movie/:movieId" element={<MovieDetails />} />
+        </Routes>
+      </main>
+    </Router>
   );
-}
-
-export default App;
+};
