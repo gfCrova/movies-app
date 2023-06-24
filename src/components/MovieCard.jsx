5f9fc541780263822a021b8a1c188319fe0b42ee
gfCrova <giancrova.cl5@gmail.com>
2023-06-24 02:26:33 -0300

    add css to movieCard comp.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import styles from "../css/MovieCard.module.css";
+import { Link } from "react-router-dom";
+import styles from "./MovieCard.module.css";
 
-const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie }) => {
   const imgUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
   return (
     <li className={styles.movieCard}>
-      <img
-        width={230}
-        height={345}
-        className={styles.movieImage}
-        src={imgUrl}
-        alt={"Image of the movie: " + movie.title}
-      />
-      {movie.title}
+      <Link className={styles.movieLink} exact to={"/movie/" + movie.id}>
+        <img
+          width={230}
+          height={345}
+          className={styles.movieImage}
+          src={imgUrl}
+          alt={"Image of the movie: " + movie.title}
+        />
+        <p className={styles.movieTitle}>{movie.title}</p>
+      </Link>
     </li>
   );
 };
-
-export default MovieCard;
