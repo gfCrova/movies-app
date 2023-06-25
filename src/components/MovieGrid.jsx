eedcf14270148cfe5d1de1bd8428a2c06e418ff7
gfCrova <giancrova.cl5@gmail.com>
2023-06-25 01:51:23 -0300

    add state isLoading and Spinner to movieGrid

--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -2,17 +2,25 @@ import { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard";
 import styles from "./MovieGrid.module.css";
 import { get } from "../Utils/httpClient";
+import { Spinner } from "./Spinner";
 
 export const MovieGrid = () => {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       const data = await get("/discover/movie");
       setMovies(data.results);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
+
+  if(isLoading) {
+    return <Spinner />
+  }
 
   return (
     <ul className={styles.moviesGrid}>
