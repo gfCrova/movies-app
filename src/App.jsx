6e902561bc033d60ee239452356e26071d587f25
gfCrova <giancrova.cl5@gmail.com>
2023-06-27 23:11:06 -0300

    Update routes -> element= GenresPage and state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { HomePage } from "./pages/HomePage";
-import { MovieDetails } from "./pages/MovieDetails.jsx";
+import { DetailsPage } from "./pages/DetailsPage.jsx";
 import styles from "./App.module.css";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Navigation } from "./components/Navigation";
+import { ImgLogo } from "./components/ItemsNav";
+import { GenresPage } from "./pages/GenresPage";
+import { useState } from "react";
 
 export const App = () => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+
+  const handleNavigationGenreSelect = (genreId) => {
+    setSelectedGenre(genreId);
+  };
+
   return (
     <Router>
       <header className={styles.appHeader}>
-        <Link className={styles.linkHeader} to="/">
-          <h1>Movies</h1>
+        <Link to={"/"}>
+          <ImgLogo />
         </Link>
+        <Navigation onGenreSelect={selectedGenre} />
       </header>
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/movie/:movieId" element={<MovieDetails />} />
+          <Route path="/movie/:movieId" element={<DetailsPage />} />
+          <Route path="/genres/:genreId" element={<GenresPage genre={handleNavigationGenreSelect}/>} />
         </Routes>
       </main>
     </Router>
