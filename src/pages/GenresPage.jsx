09d9b600b9839723bc5ec571b0e1f6a543f75eb9
gfCrova <giancrova.cl5@gmail.com>
2023-06-27 23:12:54 -0300

    renderizando div contenedor de GenresGrid and title

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { GenresGrid } from "../components/GenresGrid";
 import { useQuery } from "../hooks/useQuery";
 import { useDebounce } from "../hooks/useDebounce";
+import { useParams } from "react-router-dom";
 
 export const GenresPage = () => {
+  const { genreId } = useParams();
   const query = useQuery();
   const genre = query.get("genre");
   const debounceSearch = useDebounce(genre, 500);
+
   return (
     <div>
-      <GenresGrid key={debounceSearch} genre={debounceSearch}/>
+      <h1>Search Genres: {genreId}</h1>
+      <GenresGrid key={debounceSearch} genre={genreId}/>
     </div>
   );
 };
