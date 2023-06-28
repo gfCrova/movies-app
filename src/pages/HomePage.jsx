5078f2fff89de3f32604c6077cc6e44594bf6819
gfCrova <giancrova.cl5@gmail.com>
2023-06-27 23:12:09 -0300

    rendering MovieGrid(key, search)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import { MovieGrid } from '../components/MovieGrid'
+import React from "react";
+import { MovieGrid } from "../components/MovieGrid";
+import { useQuery } from "../hooks/useQuery";
+import { useDebounce } from "../hooks/useDebounce";
 
 export const HomePage = () => {
+  const query = useQuery();
+  const search = query.get("search");
+  const debounceSearch = useDebounce(search, 500);
   return (
-    <MovieGrid />
-  )
-}
+    <div>
+      <MovieGrid key={debounceSearch} search={debounceSearch}/>
+    </div>
+  );
+};
