07d3517c4d378176415656e698dea5b42160f982
gfCrova <giancrova.cl5@gmail.com>
2023-06-29 00:40:02 -0300

    update App and css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Navigation } from "./components/Navigation";
 import { ImgLogo } from "./components/ItemsNav";
 import { GenresPage } from "./pages/GenresPage";
 import { useState } from "react";
+import {Search} from './components/Search'
 
 export const App = () => {
   const [selectedGenre, setSelectedGenre] = useState(null);
@@ -16,19 +17,36 @@ export const App = () => {
 
   return (
     <Router>
-      <header className={styles.appHeader}>
-        <Link to={"/"}>
-          <ImgLogo />
-        </Link>
-        <Navigation onGenreSelect={selectedGenre} />
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movie/:movieId" element={<DetailsPage />} />
-          <Route path="/genres/:genreId" element={<GenresPage genre={handleNavigationGenreSelect}/>} />
-        </Routes>
-      </main>
+      <body className={styles.body}>
+        <header className={styles.appHeader}>
+          <Link to={"/"}>
+            <ImgLogo />
+          </Link>
+          <Navigation
+            selectedGenre={selectedGenre}
+            onGenreSelect={handleNavigationGenreSelect}
+          />
+          <Search/>
+        </header>
+        <main className={styles.main}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/movie/:movieId" element={<DetailsPage />} />
+            <Route
+              path="/genres/:genreId"
+              element={<GenresPage genre={selectedGenre} />}
+            />
+          </Routes>
+        </main>
+        <footer className={styles.footer}>
+          <ul className={styles.footerUl}>
+            <li>Instagram</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Github</li>
+          </ul>
+        </footer>
+      </body>
     </Router>
   );
 };
