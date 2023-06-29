0e98a6389226b0baded7f106c12e785ddc2718c5
gfCrova <giancrova.cl5@gmail.com>
2023-06-29 00:39:25 -0300

    modified carousel component, add classNames por caption

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,42 +1,42 @@
 import Carousel from 'react-bootstrap/Carousel';
-import imgIndianaJones from '../assets/img/indiana-jones.jpg'
-import imgHiddenStrike from '../assets/img/hidden-strike.jpg'
-import imgTransformers from '../assets/img/transformers.jpg'
+import imgIndianaJones from '../assets/img/indiana-jones2.png'
+import imgHiddenStrike from '../assets/img/hidden-strike2.png'
+import imgTransformers from '../assets/img/transformers2.png'
 
 export const CarouselHome = () => {
   return (
     <Carousel>
       <Carousel.Item>
         <img
-          className=""
           src={imgIndianaJones}
           alt="First slide"
+          className='w-100'
         />
-        <Carousel.Caption>
+        <Carousel.Caption className='bg-black rounded-4 opacity-75 w-50 m-auto'>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="w-50"
           src={imgHiddenStrike}
           alt="Second slide"
+          className='w-100'
         />
 
-        <Carousel.Caption>
+        <Carousel.Caption className='bg-black rounded-4 opacity-75 w-50 m-auto'>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
-          className="w-50"
           src={imgTransformers}
           alt="Third slide"
+          className='w-100'
         />
 
-        <Carousel.Caption>
+        <Carousel.Caption className='bg-black rounded-4 opacity-75 w-50 m-auto'>
           <h3>Third slide label</h3>
           <p>
             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
