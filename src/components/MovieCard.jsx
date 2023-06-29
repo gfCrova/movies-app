9683695fad60514a6bb4a5519a1c1c8251d2772c
gfCrova <giancrova.cl5@gmail.com>
2023-06-29 00:41:31 -0300

    update grid and cards from movies

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./MovieCard.module.css";
+import { moviePoster } from "../Utils/moviePoster";
+
+/**
+  Componente MovieCard
+ 
+  Renderiza una tarjeta de película con su imagen y título.
+  La tarjeta se muestra dentro de un enlace que redirige a la página de detalles de la película.
+ 
+  Props:
+  - movie: Objeto que contiene los datos de la película.
+ 
+  Ejemplo de uso:
+ 
+  <MovieCard movie={movieData} />
+*/
 
 export const MovieCard = ({ movie }) => {
-  const imgUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
+  const imgUrl = moviePoster(movie.poster_path, 300);
   return (
     <li className={styles.movieCard}>
       <Link className={styles.movieLink} to={"/movie/" + movie.id}>
--- src/components/MovieGrid.jsx
+++ src/components/MovieGrid.jsx
@@ -1,32 +1,58 @@
 import { useEffect, useState } from "react";
 import { MovieCard } from "./MovieCard";
-import styles from "./MovieGrid.module.css";
 import { get } from "../Utils/httpClient";
 import { Spinner } from "./Spinner";
+import InfiniteScroll from "react-infinite-scroll-component";
+import styles from "./MovieGrid.module.css";
+import { Empty } from "./Empty";
 
-export const MovieGrid = () => {
+/**
+Componente <MovieGrid/>
+
+Renderiza una cuadrícula de películas con carga infinita (infinite scroll)
+basada en una consulta de búsqueda o descubrimiento de películas.
+*/
+
+export const MovieGrid = ({ search }) => {
   const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [hasMore, setHasMore] = useState(true); // Indicador de si hay más películas por cargar
+  const [page, setPage] = useState(1);  // Número de página actual
 
   useEffect(() => {
-    setIsLoading(true);
+
     const fetchData = async () => {
-      const data = await get("/discover/movie");
-      setMovies(data.results);
-      setIsLoading(false);
+      // Construir la URL de la consulta en función de la búsqueda y la página actual
+      let searchUrl = search ? `/search/movie?query=${search}&page=${page}` : `/discover/movie?page=${page}`;
+
+      const data = await get(searchUrl);
+
+      // Filtrar películas duplicadas
+      const uniqueMovies = data.results.filter(
+        (movie) => !movies.some((m) => m.id === movie.id)
+      );
+      // Actualizar la lista de películas y el indicador de carga
+      setMovies((prevMovies) => prevMovies.concat(uniqueMovies));
+      setHasMore(data.page < data.total_pages);
     };
     fetchData();
-  }, []);
+  }, [search, page, movies]);
 
-  if(isLoading) {
-    return <Spinner />
+  if(movies.length === 0 && !hasMore) {
+    return <Empty/>
   }
 
   return (
-    <ul className={styles.moviesGrid}>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </ul>
+    <InfiniteScroll
+      dataLength={movies.length} 
+      hasMore={hasMore}  // Indicador de si hay más películas por cargar
+      next={() => setPage((prevPage) => prevPage + 1)} // Función para cargar más películas
+      loader={<Spinner />} // Componente de carga mientras se obtienen más películas
+    >
+      <ul className={styles.moviesGrid}>
+        {movies.map((movie, index) => (
+          <MovieCard key={`${movie.id}-${index}`} movie={movie} /> // Renderizar cada película en un componente MovieCard
+        ))}
+      </ul>
+    </InfiniteScroll>
   );
 };
