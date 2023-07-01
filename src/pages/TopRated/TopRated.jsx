a4080a2e359d37b121c5f9c182e008e46aa02e04
gfCrova <giancrova.cl5@gmail.com>
2023-07-01 02:20:43 -0300

    new route TopRated

---
+++ src/pages/TopRated/TopRated.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MovieGridBase } from '../../components/Movie/MovieGridBase/MovieGridBase'
+import styles from '../Genres/GenresPage.module.css'
+import { FaRankingStar } from 'react-icons/fa6';
+
+export const TopRated = () => {
+  return (
+    <div>
+        <h1 className={styles.title}>Top Rated <FaRankingStar color='yellow'/></h1>
+        <MovieGridBase topRated/>
+    </div>
+  )
+}
