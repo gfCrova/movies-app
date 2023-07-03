3953c8defd29f06862918440018f26988f1477b4
gfCrova <giancrova.cl5@gmail.com>
2023-07-03 10:33:42 -0300

    agregando el componente Search

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { MovieGridBase } from "../../components/Movie/MovieGridBase/MovieGridBase";
 import { useQuery } from "../../hooks/useQuery";
 import { useDebounce } from "../../hooks/useDebounce";
-import { CarouselHome } from '../../components/Carousel/Carousel'
+import { CarouselHome } from "../../components/Carousel/Carousel";
+import { Search } from "../../components/Navigation/Search/Search";
 
 export const HomePage = () => {
   const query = useQuery();
@@ -10,8 +11,11 @@ export const HomePage = () => {
   const debounceSearch = useDebounce(search, 500);
   return (
     <div>
-      <CarouselHome/>
-      <MovieGridBase key={debounceSearch} search={debounceSearch}/>
+      <CarouselHome />
+      <div className="p-4">
+        <Search />
+      </div>
+      <MovieGridBase key={debounceSearch} search={debounceSearch} />
     </div>
   );
 };
