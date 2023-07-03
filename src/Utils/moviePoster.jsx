9c3a9da320aa9a2f539dc707c53909140f467335
gfCrova <giancrova.cl5@gmail.com>
2023-07-02 21:02:45 -0300

    new path backdrop and export

--- src/Utils/moviePoster.jsx
+++ src/Utils/moviePoster.jsx
@@ -3,3 +3,7 @@ import placeholder from "../assets/img/placeholder.jpg"
 export const moviePoster = (path, width) => { 
     return path ? `https://image.tmdb.org/t/p/w${width}${path}` : placeholder;
 }
+
+export const backdropPath = (path, width) => {
+    return path ? `https://image.tmdb.org/t/p/w${width}${path}` : placeholder;
+}
