7bd8fd63786d7003fdca975838a46115f7c45ac0
gfCrova <giancrova.cl5@gmail.com>
2023-07-03 10:32:50 -0300

    haciendo responsive el carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,48 +1,52 @@
-import Carousel from 'react-bootstrap/Carousel';
-import imgIndianaJones from '../../assets/img/indiana-jones2.png'
-import imgHiddenStrike from '../../assets/img/hidden-strike2.png'
-import imgTransformers from '../../assets/img/transformers2.png'
+import Carousel from "react-bootstrap/Carousel";
+import imgIndianaJones from "../../assets/img/indiana-jones2.png";
+import imgHiddenStrike from "../../assets/img/hidden-strike2.png";
+import imgTransformers from "../../assets/img/transformers2.png";
+import { Badge } from "react-bootstrap";
+import styles from './Carousel.module.css'
 
 export const CarouselHome = () => {
   return (
     <Carousel>
-      <Carousel.Item>
-        <img
-          src={imgIndianaJones}
-          alt="First slide"
-          className='w-100'
-        />
-        <Carousel.Caption className='bg-black rounded-4 opacity-75 w-50 m-auto'>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      <Carousel.Item className={styles.imgCarousel}>
+        <img src={imgIndianaJones} alt="First slide" className={`w-100 opacity-50`} />
+        <Carousel.Caption className={`${styles.caption} w-75`}>
+          <h1 className={`${styles.captionTitle} text-start text-light fw-bold pb-2`}>Indiana Jones y el dial del destino (2023)<Badge className="ms-2 p-1 bg-warning text-dark">Movie</Badge></h1>
+          <p className="text-start">
+            El arqueólogo Indiana Jones deberá emprender otra aventura contra el
+            tiempo para intentar recuperar un dial legendario que puede cambiar
+            el curso de la historia. Acompañado por su ahijada, Jones pronto se
+            encuentra enfrentándose a Jürgen Voller, un ex nazi que trabaja para
+            la NASA.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          src={imgHiddenStrike}
-          alt="Second slide"
-          className='w-100'
-        />
-
-        <Carousel.Caption className='bg-black rounded-4 opacity-75 w-50 m-auto'>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <Carousel.Item className={styles.imgCarousel}>
+        <img src={imgHiddenStrike} alt="Second slide" className={`w-100 opacity-50`} />
+        <Carousel.Caption className={`${styles.caption} w-75`}>
+        <h1 className={`${styles.captionTitle} text-start text-light fw-bold pb-2`}>Indiana Jones y el dial del destino (2023)<Badge className="ms-2 p-1 bg-warning text-dark">Movie</Badge></h1>
+          <p className="text-start">
+            El arqueólogo Indiana Jones deberá emprender otra aventura contra el
+            tiempo para intentar recuperar un dial legendario que puede cambiar
+            el curso de la historia. Acompañado por su ahijada, Jones pronto se
+            encuentra enfrentándose a Jürgen Voller, un ex nazi que trabaja para
+            la NASA.
+          </p>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          src={imgTransformers}
-          alt="Third slide"
-          className='w-100'
-        />
-
-        <Carousel.Caption className='bg-black rounded-4 opacity-75 w-50 m-auto'>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+      <Carousel.Item className={styles.imgCarousel}>
+        <img src={imgTransformers} alt="Third slide" className={`w-100 opacity-50`} />
+        <Carousel.Caption className={`${styles.caption} w-75`}>
+        <h1 className={`${styles.captionTitle} text-start text-light fw-bold pb-2`}>Indiana Jones y el dial del destino (2023)<Badge className="ms-2 p-1 bg-warning text-dark">Movie</Badge></h1>
+          <p className="text-start">
+            El arqueólogo Indiana Jones deberá emprender otra aventura contra el
+            tiempo para intentar recuperar un dial legendario que puede cambiar
+            el curso de la historia. Acompañado por su ahijada, Jones pronto se
+            encuentra enfrentándose a Jürgen Voller, un ex nazi que trabaja para
+            la NASA.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
-}
+};
