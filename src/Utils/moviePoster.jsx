e7c6bc66e7f05b1be5d5267ca7a687b0afef2c27
gfCrova <giancrova.cl5@gmail.com>
2023-07-05 01:52:23 -0300

    import img-notFound and modified return function backdropPath

--- src/Utils/moviePoster.jsx
+++ src/Utils/moviePoster.jsx
@@ -1,9 +1,10 @@
 import placeholder from "../assets/img/placeholder.jpg"
+import back from '../assets/img/back-notFound.png'
 
 export const moviePoster = (path, width) => { 
     return path ? `https://image.tmdb.org/t/p/w${width}${path}` : placeholder;
 }
 
 export const backdropPath = (path, width) => {
-    return path ? `https://image.tmdb.org/t/p/w${width}${path}` : placeholder;
+    return path ? `https://image.tmdb.org/t/p/w${width}${path}` : back;
 }
