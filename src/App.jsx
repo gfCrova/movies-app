27428fc78cee511b4d42242f0987c48810d17c68
gfCrova <giancrova.cl5@gmail.com>
2023-07-05 01:50:48 -0300

    move navigation for header, add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { HomePage } from "./pages/Home/HomePage";
 import { DetailsPage } from "./pages/Details/DetailsPage.jsx";
 import styles from "./App.module.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { Navigation } from "./components/Navigation/Navigation";
 import { GenresPage } from "./pages/Genres/GenresPage";
 import { useState } from "react";
@@ -19,33 +24,24 @@ export const App = () => {
   return (
     <div className={styles.body}>
       <Router>
-        <header className={styles.appHeader}>
+        <header className={styles.navHeader}>
           <Navigation
             selectedGenre={selectedGenre}
             onGenreSelect={handleNavigationGenreSelect}
           />
         </header>
-        <main className={styles.main}>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movie/:movieId" element={<DetailsPage />} />
-            <Route
-              path="/genres/:genreId"
-              element={<GenresPage genreId={selectedGenre} />}
-            />
-            <Route path="/movie/top_rated" element={<TopRated />} />
-            <Route path="/movie/:movieId/videos" element={<VideoGrid />} />
-            <Route path="/movie/upcoming" element={<Upcoming />} />
-          </Routes>
-        </main>
-        <footer className={styles.footer}>
-          <ul className={styles.footerUl}>
-            <li>Instagram</li>
-            <li>Facebook</li>
-            <li>Twitter</li>
-            <li>Github</li>
-          </ul>
-        </footer>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movie/:movieId" element={<DetailsPage />} />
+          <Route
+            path="/genres/:genreId"
+            element={<GenresPage genreId={selectedGenre} />}
+          />
+          <Route path="/movie/top_rated" element={<TopRated />} />
+          <Route path="/movie/:movieId/videos" element={<VideoGrid />} />
+          <Route path="/movie/upcoming" element={<Upcoming />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
+        </Routes>
       </Router>
     </div>
   );
