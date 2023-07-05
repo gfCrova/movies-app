4efdf02e85ce52834733027a3d10bb5450ac67f6
gfCrova <giancrova.cl5@gmail.com>
2023-07-05 01:54:59 -0300

    update pages details and upcoming

--- src/pages/Upcoming/Upcoming.jsx
+++ src/pages/Upcoming/Upcoming.jsx
@@ -2,7 +2,7 @@ import { MovieGridBase } from '../../components/Movie/MovieGridBase/MovieGridBas
 
 export const Upcoming = () => {
   return (
-    <div className='pt-5'>
+    <div>
         <h1 className="text-light text-center py-5">Upcoming: </h1>
         <MovieGridBase upcoming={true}/>
     </div>
