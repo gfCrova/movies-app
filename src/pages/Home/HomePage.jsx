824216d5f51796be5e80bd788a0472b6d18aa900
gfCrova <giancrova.cl5@gmail.com>
2023-07-07 23:18:58 -0300

    agregando estado de carga, cambiando el background del home

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,29 +1,21 @@
-import React from "react";
 import { MovieGridBase } from "../../components/Movie/MovieGridBase/MovieGridBase";
 import { useQuery } from "../../hooks/useQuery";
 import { useDebounce } from "../../hooks/useDebounce";
 import { CarouselHome } from "../../components/Carousel/Carousel";
-import { Search } from "../../components/Search/Search";
 import styles from './HomePage.module.css'
-import logoImg from '../../assets/img/logo-tmdb.svg'
-import logoTitle from '../../assets/img/logoTitle.png'
+import image from '../../assets/img/logoTitle.png'
+import { useState } from "react";
 
 export const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const query = useQuery();
   const search = query.get("search");
   const debounceSearch = useDebounce(search, 500);
+
   return (
-    <div>
-      <h1 className={styles.titleHome}>
-        goFlix
-        <img src={logoTitle} alt="logoTitle" />
-      </h1>
+    <div className={styles.divBg} onChange={() => {setIsLoading(false)}}>
+      <h1 className={styles.title}>GoFlix <img className={styles.imgLogo} src={image} alt="logo"/></h1>
       <CarouselHome />
-      <div className="d-flex justify-content-around gap-3 py-4">
-        <img className={styles.img} src={logoImg} alt="logo" width={300} />
-        <Search />
-        <img className={styles.img} src={logoImg} alt="logo" width={300} />
-      </div>
       <MovieGridBase key={debounceSearch} search={debounceSearch} />
     </div>
   );
