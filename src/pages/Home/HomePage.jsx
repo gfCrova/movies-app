19ef571c61a157881526e0ee63f98dbbac79436d
gfCrova <giancrova.cl5@gmail.com>
2023-07-14 22:54:56 -0300

    refactorizando el code. quitando use state y método onChange con isLoading

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -4,16 +4,15 @@ import { useDebounce } from "../../hooks/useDebounce";
 import { CarouselHome } from "../../components/Carousel/Carousel";
 import styles from './HomePage.module.css'
 import image from '../../assets/img/logoTitle.png'
-import { useState } from "react";
 
 export const HomePage = () => {
-  const [isLoading, setIsLoading] = useState(true);
+
   const query = useQuery();
   const search = query.get("search");
   const debounceSearch = useDebounce(search, 500);
 
   return (
-    <div className={styles.divBg} onChange={() => {setIsLoading(false)}}>
+    <div className={styles.divBg}>
       <h1 className={styles.title}>GovaFlix <img className={styles.imgLogo} src={image} alt="logo"/></h1>
       <CarouselHome />
       <MovieGridBase key={debounceSearch} search={debounceSearch} />
