e5de174a91b27ec5c6611ef92dc90e61171b5fe3
gfCrova <giancrova.cl5@gmail.com>
2023-07-14 22:56:03 -0300

    delete classname .btnItem all navbarItems

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -80,140 +80,120 @@ export const Navigation = ({ selectedGenre, onGenreSelect }) => {
                 id="basic-nav-dropdown"
               >
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={28}
                   active={selectedGenre === 28}
                 >
                   Action
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={12}
                   active={selectedGenre === 12}
                 >
                   Adventure
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={16}
                   active={selectedGenre === 16}
                 >
                   Animation
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={35}
                   active={selectedGenre === 35}
                 >
                   Comedy
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={80}
                   active={selectedGenre === 80}
                 >
                   Crime
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={99}
                   active={selectedGenre === 99}
                 >
                   Documentary
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={18}
                   active={selectedGenre === 18}
                 >
                   Drama
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={10751}
                   active={selectedGenre === 10751}
                 >
                   Family
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={14}
                   active={selectedGenre === 14}
                 >
                   Fantasy
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={36}
                   active={selectedGenre === 36}
                 >
                   History
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={27}
                   active={selectedGenre === 27}
                 >
                   Horror
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={36}
                   active={selectedGenre === 36}
                 >
                   Musik
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={10402}
                   active={selectedGenre === 10402}
                 >
                   History
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={9648}
                   active={selectedGenre === 9648}
                 >
                   Mystery
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={10749}
                   active={selectedGenre === 10749}
                 >
                   Romance
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={878}
                   active={selectedGenre === 878}
                 >
                   Science Fiction
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={10770}
                   active={selectedGenre === 10770}
                 >
                   TV-Film
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={53}
                   active={selectedGenre === 53}
                 >
                   Thriller
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={10752}
                   active={selectedGenre === 10752}
                 >
                   War Movie
                 </NavDropdown.Item>
                 <NavDropdown.Item
-                  className={`${styles.btnItem}`}
                   eventKey={37}
                   active={selectedGenre === 37}
                 >
