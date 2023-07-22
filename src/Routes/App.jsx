1c4469c767d0767fd3e3c13212e04aff319058f5
gfCrova <giancrova.cl5@gmail.com>
2023-07-21 23:32:14 -0300

    change route /series redirect to: SeriesPage

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,5 +1,5 @@
 import { HomePage } from "../pages/Home/HomePage";
-import { DetailsPage } from "../pages/Details/DetailsPage.jsx";
+import { MovieDetails } from "../pages/Details/MovieDetails.jsx";
 import {
   BrowserRouter as Router,
   Routes,
@@ -11,7 +11,7 @@ import { TopRated } from "../pages/TopRated/TopRated";
 import { VideoGrid } from "../components/Movie/VideoGrid/VideoGrid";
 import { Upcoming } from "../pages/Upcoming/Upcoming";
 import Layout from "../components/Layout/Layout";
-import { SerieGrid } from "../components/Serie/SerieGrid";
+import { SeriesPage } from "../pages/Series/SeriesPage";
 
 export const App = () => {
   return (
@@ -19,12 +19,12 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="movie/:movieId" element={<DetailsPage />} />
-          <Route path="genres/:genreId" element={<GenresPage />} />
-          <Route path="movie/top_rated" element={<TopRated />} />
-          <Route path="movie/:movieId/videos" element={<VideoGrid />} />
           <Route path="movie/upcoming" element={<Upcoming />} />
-          <Route path="serie" element={<SerieGrid/>}></Route>
+          <Route path="movie/top_rated" element={<TopRated />} />
+          <Route path="genres/:genreId" element={<GenresPage />} />
+          <Route path="series" element={<SeriesPage/>}></Route>
+          <Route path="movie/:movieId" element={<MovieDetails />} />
+          <Route path="movie/:movieId/videos" element={<VideoGrid />} />
           <Route path="*" element={<Navigate replace to="/" />} />{" "}
           {/* Cualquier Ruta inexistente, Redirecciona a Home */}
         </Route>
